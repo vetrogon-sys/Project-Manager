44117e7d7a2c316ee0ff7e4fe9963530cc86036c
KostyaBogdanov <kostyabogdanov338@gmail.com>
2023-01-11 11:30:02 +0300

    rework services  - reduce cont of data base requests

--- src-be/src/main/java/com/example/service/impl/TaskServiceImpl.java
+++ src-be/src/main/java/com/example/service/impl/TaskServiceImpl.java
@@ -29,6 +29,12 @@ public class TaskServiceImpl implements TaskService {
     }
 
     @Override
+    public Task getById(Long taskId) {
+        return taskRepository.findById(taskId)
+              .orElseThrow(() -> new EntityNotFoundException(String.format("Can't find Task with id: %d", taskId)));
+    }
+
+    @Override
     public void deleteById(Long id) {
         if (!taskRepository.existsById(id)) {
             throw new EntityNotFoundException(String.format("Task with id: %d doesn't exist", id));
