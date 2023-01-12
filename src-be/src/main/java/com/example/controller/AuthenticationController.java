0b6ed836f38963fa1f094d5cdbeef0d7007baea1
KostyaBogdanov <kostyabogdanov338@gmail.com>
2023-01-12 15:23:34 +0300

    rework response of AuthorizationController

--- src-be/src/main/java/com/example/controller/AuthenticationController.java
+++ src-be/src/main/java/com/example/controller/AuthenticationController.java
@@ -1,9 +1,11 @@
 package com.example.controller;
 
 import com.example.dto.AuthorizationDto;
+import com.example.dto.JWTTokenDto;
 import com.example.dto.RegistrationDto;
 import com.example.service.AuthorizationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,19 +23,19 @@ public class AuthenticationController {
     private final AuthorizationService authorizationService;
 
     @PostMapping("/signin")
-    public String login(@Valid @RequestBody AuthorizationDto authorizationDto) {
-        return authorizationService.signIn(authorizationDto);
+    public ResponseEntity<JWTTokenDto> login(@Valid @RequestBody AuthorizationDto authorizationDto) {
+        return ResponseEntity.ok(authorizationService.signIn(authorizationDto));
     }
 
     @PostMapping("/signup")
     @PermitAll
-    public String register(@Valid @RequestBody RegistrationDto registrationDto) {
-        return authorizationService.signUp(registrationDto);
+    public ResponseEntity<JWTTokenDto> register(@Valid @RequestBody RegistrationDto registrationDto) {
+        return ResponseEntity.ok(authorizationService.signUp(registrationDto));
     }
 
     @PostMapping("/refresh")
-    public String refresh(HttpServletRequest request) {
-        return authorizationService.refreshAuthentication(request.getRemoteUser());
+    public ResponseEntity<JWTTokenDto> refresh(HttpServletRequest request) {
+        return ResponseEntity.ok(authorizationService.refreshAuthentication(request.getRemoteUser()));
     }
 
 }
