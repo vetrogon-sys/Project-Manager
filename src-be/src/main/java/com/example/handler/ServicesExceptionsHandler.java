0adfae78eb9327edee10ad58888e0ec600132b5b
KostyaBogdanov <kostyabogdanov338@gmail.com>
2023-01-12 14:17:42 +0300

    add authentication exceptions into handler

--- src-be/src/main/java/com/example/handler/ServicesExceptionsHandler.java
+++ src-be/src/main/java/com/example/handler/ServicesExceptionsHandler.java
@@ -1,7 +1,9 @@
 package com.example.handler;
 
 import com.example.dto.ExceptionResponse;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
+import com.example.exeptions.ExpiredTokenException;
+import com.example.exeptions.InvalidCredentialsException;
+import com.example.exeptions.WrongAuthenticationTokenException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -32,13 +35,65 @@ public class ServicesExceptionsHandler extends ResponseEntityExceptionHandler {
         response.setPath(request.getRequestURI());
         response.setStatus(HttpStatus.BAD_REQUEST.value());
         response.setTimestamp(LocalDateTime.now());
-        response.setErrors(new LinkedHashMap<>() {{
-            put(STRING_VALUE_ERROR, exception.getMessage());
-        }});
+
+        Map<String, String> errors = getErrorsMapFromException(exception);
+
+        response.setErrors(errors);
 
         return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(response);
+              .status(HttpStatus.BAD_REQUEST)
+              .body(response);
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ExceptionResponse> handleInvalidCredentialsException(InvalidCredentialsException exception,
+                                                                               HttpServletRequest request) {
+        ExceptionResponse response = new ExceptionResponse();
+        response.setPath(request.getRequestURI());
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setTimestamp(LocalDateTime.now());
+
+        Map<String, String> errors = getErrorsMapFromException(exception);
+
+        response.setErrors(errors);
+
+        return ResponseEntity
+              .status(HttpStatus.FORBIDDEN)
+              .body(response);
+    }
+
+    @ExceptionHandler(WrongAuthenticationTokenException.class)
+    public ResponseEntity<ExceptionResponse> handleWrongAuthenticationTokenException(WrongAuthenticationTokenException exception,
+                                                                                     HttpServletRequest request) {
+        ExceptionResponse response = new ExceptionResponse();
+        response.setPath(request.getRequestURI());
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setTimestamp(LocalDateTime.now());
+
+        Map<String, String> errors = getErrorsMapFromException(exception);
+
+        response.setErrors(errors);
+
+        return ResponseEntity
+              .status(HttpStatus.FORBIDDEN)
+              .body(response);
+    }
+
+    @ExceptionHandler(ExpiredTokenException.class)
+    public ResponseEntity<ExceptionResponse> handleExpiredTokenException(ExpiredTokenException exception,
+                                                                         HttpServletRequest request) {
+        ExceptionResponse response = new ExceptionResponse();
+        response.setPath(request.getRequestURI());
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setTimestamp(LocalDateTime.now());
+
+        Map<String, String> errors = getErrorsMapFromException(exception);
+
+        response.setErrors(errors);
+
+        return ResponseEntity
+              .status(HttpStatus.FORBIDDEN)
+              .body(response);
     }
 
     @Override
@@ -50,13 +105,19 @@ public class ServicesExceptionsHandler extends ResponseEntityExceptionHandler {
         response.setStatus(HttpStatus.BAD_REQUEST.value());
         response.setTimestamp(LocalDateTime.now());
         response.setErrors(ex.getAllErrors().stream()
-                .collect(Collectors.toMap(
-                        e -> ((FieldError) e).getField(),
-                        e -> Optional.ofNullable(e.getDefaultMessage()).orElse(DEFAULT_EXCEPTION_MASSAGE)
-                )));
+              .collect(Collectors.toMap(
+                    e -> ((FieldError) e).getField(),
+                    e -> Optional.ofNullable(e.getDefaultMessage()).orElse(DEFAULT_EXCEPTION_MASSAGE)
+              )));
 
         return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(response);
+              .status(HttpStatus.BAD_REQUEST)
+              .body(response);
+    }
+
+    private Map<String, String> getErrorsMapFromException(RuntimeException exception) {
+        Map<String, String> errors = new LinkedHashMap<>();
+        errors.put(STRING_VALUE_ERROR, exception.getMessage());
+        return errors;
     }
 }
