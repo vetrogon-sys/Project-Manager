d52d602bf652aff3b506e4b3058eb6ed4a5cfbf4
KostyaBogdanov <kostyabogdanov338@gmail.com>
2023-05-10 11:56:10 +0300

    project elements styles

---
+++ src-fe/src/components/project/ProjectElement.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { Card, CardContent, CardActions, Button, Typography } from '@mui/material';
+
+export default function ProjectElement(project) {
+
+    return (
+        <Card sx={{ minWidth: 275, margin: 2, textAlign: 'left' }}>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Project {project.id}
+                </Typography>
+                <Typography variant="h5" component="div">
+                    {project.name}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    adjective
+                </Typography>
+                <Typography variant="body2">
+                    {project.description ? project.description : 'There isn\'t description here...'}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">See project</Button>
+            </CardActions>
+        </Card>
+    )
+}
