4305f49affb830e09e2a8f23c7cc6398cd9a3e38
KostyaBogdanov <kostyabogdanov338@gmail.com>
2023-05-27 10:13:44 +0300

    create oauth2 maven module

---
+++ oauth2-client/src/main/java/org/example/oauth/OAuth2ClientApplication.java
@@ -0,0 +1,13 @@
+package org.example.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2ClientApplication.class, args);
+    }
+
+}
